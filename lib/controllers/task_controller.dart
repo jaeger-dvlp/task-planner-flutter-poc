f0c9393f012b3969fe0dd0f0f66d5d7ee6ec45a1
jaeger-dvlp <omer.kayalar0101@icloud.com>
2025-05-29 20:52:52 +0300

    feat: create task controller

---
+++ lib/controllers/task_controller.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
+
+import 'package:task_planner_poc/models/task_model.dart';
+
+class TaskController extends GetxController {
+  final RxList<Task> taskList = <Task>[].obs;
+  final Uuid uuid = Uuid();
+
+  void addTask(String title, String description) {
+    final newTask = Task(
+      id: uuid.v4(),
+      title: title,
+      description: description,
+      createdAt: DateTime.now(),
+    );
+
+    taskList.add(newTask);
+    update();
+  }
+
+  void removeTask(String id) {
+    taskList.removeWhere((task) => task.id == id);
+    update();
+  }
+
+  void toggleDone(String id) {
+    final index = taskList.indexWhere((task) => task.id == id);
+
+    if (index != -1) {
+      final updatedTask = taskList[index].copyWith(
+        isDone: !taskList[index].isDone,
+      );
+      taskList[index] = updatedTask;
+      update();
+    }
+  }
+}
